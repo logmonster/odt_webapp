f70ca81d8d09baca0eb0a2f7548714d2c68a48ca
jason.wong <jason.wong@jasonwongs-MacBook-Pro.local>
2018-01-04 13:04:52 +0800

    2018-01-04a

---
+++ apps/jeymartECommerce/script/setup/setEnvVariable.sh
@@ -0,0 +1,10 @@
+
+export es_host=http://localhost:9201
+
+export source_dir=/Users/jason.wong/Development/elastic/odt_webapp/apps/jeymartECommerce/script/setup/
+export item_data=jeymartItemData.json
+export item_template=jeymart_item_template.json
+
+export user_trx_data=jeymartUserTrxData.json
+export user_trx_template=jeymart_user_trx_template.json
+
