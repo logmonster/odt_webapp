3c75847561848bf6b85985e80f55568e8c8d048e
jason.wong <jason.wong@jasonwongs-MacBook-Pro.local>
2018-01-18 15:03:06 +0800

    2018-01-18a

---
+++ apps/nycTaxiHub/script/setup/setEnvVariable.sh
@@ -0,0 +1,8 @@
+
+export es_host=http://localhost:9201
+
+export source_dir=/Users/jason.wong/Development/elastic/odt_webapp/apps/nycTaxiHub/script/setup/
+
+export taxi_data=nycTaxiHubData.json
+export taxi_template=nyc_taxi_hub_template.json
+
