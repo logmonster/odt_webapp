6df25fdaa0c2c78327301990b747cf93ef5554f6
jason.wong <jason.wong@jasonwongs-MacBook-Pro.local>
2018-03-08 07:41:27 +0800

    2018-03-08a

--- apps/labEngine/public/clientView/jscript/engine.js
+++ apps/labEngine/public/clientView/jscript/engine.js
@@ -40,6 +40,7 @@ function initQueryButtons() {
       // console.log(JSON.parse(CONST_SIMPLE_TERMS_AGGS.query));
       // console.log(CONST_SIMPLE_TERMS_AGGS.query);
       copyCodeToQueryTextArea(CONST_SIMPLE_MATCH_QUERY);
+      window.queryType = 'query';
     });
   }
 
@@ -47,6 +48,7 @@ function initQueryButtons() {
   if (_jBtn.length>0) {
     _jBtn.on('click', function(_event) {
       copyCodeToQueryTextArea(CONST_SIMPLE_TERMS_AGGS);
+      window.queryType = 'aggs';
     });
   }
 
@@ -54,6 +56,7 @@ function initQueryButtons() {
   if (_jBtn.length>0) {
     _jBtn.on('click', function(_event) {
       clearQueryTextArea();
+      window.queryType = undefined;
     });
   }
 
@@ -136,9 +139,21 @@ function _handleQueryResponse(_data, _status, _jqXHR) {
   let _resultFormatVal = _resultFormat.val();
 
   if (_resultFormatVal=='0') {
-    _renderTableFromData(_data);
+    if (window.queryType) {
+      if (window.queryType == 'query') {
+        _renderTableFromData(_data);
+      } else if (window.queryType == 'aggs') {
+        _renderTableFromData_aggs(_data);
+      }
+    }
   } else if (_resultFormatVal=='1') {
-    _renderPlainFromData(_data);
+    if (window.queryType) {
+      if (window.queryType == 'query') {
+        _renderPlainFromData(_data);
+      } else if (window.queryType == 'aggs') {
+        _renderPlainFromData_aggs(_data);
+      }
+    }
   } else if (_resultFormatVal=='2') {
     _renderJScriptSnippetWithData(_data);
   } else {
@@ -224,6 +239,61 @@ function _renderTableFromData(_data) {
     } // has data
   } // if (_dResult is valid)
 }
+function _renderTableFromData_aggs(_data) {
+  let _dResult = _getDivResult();
+  if (_dResult.length==1) {
+    _dResult.empty();
+
+    if (_data['data'].hasOwnProperty('aggregations')) {
+      let _t = '';
+      let _as = _data['data']['aggregations'];
+      // aggs is very flexible in the no. of aggs run plus the aggs_name returned
+      let _aggsLabels = Object.keys(_as);
+
+      _aggsLabels.forEach(function(_aggsL, _idx) {
+        let _aggsC = _as[_aggsL];
+        // aggs could be "buckets" or "value" based...
+        let _aggsCBuckets = _aggsC['buckets'];
+        let _aggsCValue = _aggsC['value'];
+        // table header and contents is of coz "case by case"
+        _t+="<div> &nbsp; aggs: "+_aggsL+"</div>";
+        if (_aggsCBuckets && _aggsCBuckets.length>0) {
+          // header
+          _t += "<table class='table'> <thead class='thead-inverse'><tr>";
+          // get the 1st row of data
+          let _row0Keys = Object.keys(_aggsCBuckets[0]);
+          _row0Keys.forEach(function(_rowItem, _idx2) {
+            _t += "<th>"+_rowItem+"</th>";
+          });
+          _t += "</tr></thead>";
+
+          // table content
+          _t += "<tbody>";
+          _aggsCBuckets.forEach(function(_row, _idx2) {
+            _t += "<tr>";
+            _row0Keys.forEach(function(_rowItem, _idx2) {
+              _t += "<td>"+_row[_rowItem]+"</td>";
+            });
+            _t += "</tr>";
+          });
+
+          _t += "</tbody>";
+          _t += "</table>";
+
+        } else if (_aggsCValue) {
+          _t += "<table class='table'> <thead class='thead-inverse'><tr> <th>value</th> </tr></thead>";
+          _t += "<tbody> <tr><td>"+_aggsCValue+"</td></tr> </tbody>";
+          _t += "</table>";
+
+        } else {
+          _t += "<div>no aggregation result available!</div>"
+        }
+      }); // forEach label of aggs
+      _dResult.html(_t);
+    }
+
+  } // end if (_dResult valid)
+}
 
 function _renderPlainFromData(_data) {
   let _dResult = _getDivResult();
@@ -241,6 +311,37 @@ function _renderPlainFromData(_data) {
       _dResult.html("<textarea readonly class='txtCodeView_textarea'>"+_v+"</textarea>");
     }
   } // end if (valid _dResult)
+}
+function _renderPlainFromData_aggs(_data) {
+  let _dResult = _getDivResult();
+  if (_dResult.length==1) {
+    _dResult.empty();
+
+    if (_data['data'].hasOwnProperty('aggregations')) {
+      let _d = _data['data']['aggregations'];
+      let _aKeys = Object.keys(_d);
+      let _v = '';
+
+      _aKeys.forEach(function(_aKey, _idx2) {
+        // results could be in buckets or value form
+        let _vC = _d[_aKey]['buckets'];
+        let _vV = _d[_aKey]['value'];
+
+        _v += 'aggs: '+_aKey+'\r\n';
+        if (_vC) {
+          _v += _simplePrettyJSON_stringify(_vC);
+
+        } else if (_vV) {
+          _v += '{ value: ';
+          _v += _simplePrettyJSON_stringify(_vV);
+          _v += ' }';
+        }
+        _v += "\r\n\r\n";
+      });
+      _v = "<textarea readonly class='txtCodeView_textarea'>"+_v+"</textarea>";
+      _dResult.html(_v);
+    }
+  } // end -- if (_dResult valid)
 }
 
 function _renderJScriptSnippetWithData(_data) {
